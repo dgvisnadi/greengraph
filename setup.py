5f7a24d16647a5939d702f001044efcb04ba2677
Dimitri Visnadi <dgvisnadi@gmail.com>
2016-12-14 12:19:17 +0000

    added citation file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'GreenGraph',
+    version = '0.1.0',
+    packages = find_packages(exclude=['*tests']),
+    scripts = ['scripts/greengraph'],
+    install_requires = ['argparse', 'geopy', 'matplotlib', 'mock', 'numpy', 'requests']
+)
