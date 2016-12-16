353fa6bc1ff04fcc92a66e53d308bbc1b5a182ef
Dimitri Visnadi <dgvisnadi@gmail.com>
2016-12-16 14:13:43 +0000

    Plot generating py file

---
+++ greengraph_code/greengraph.py
@@ -0,0 +1,32 @@
+from matplotlib import pyplot as plt
+from graph import Greengraph
+from map  import Map
+
+def greengraph(start, end, steps, out):
+    ''' Generate a plot showing intensiity of green pixels between two locations.
+
+    Parameters
+    ---------
+    start: str
+        Start location name, such as London or Paris
+
+    end: str
+        End location name, such as Milan or Madrid
+
+    steps: int
+        Number of images between start location and end location
+
+    out: str
+        Save as, i.e Desktop/graph.png
+
+    Returns
+    ---------
+    image
+
+    '''
+
+
+    mygraph=Greengraph(start ,end)
+    data=mygraph.green_between(steps)
+    plt.plot(data)
+    show()
