ec9a1a4ab82322e4cbf7dd1a1421475adaef96a7
Dimitri Visnadi <dgvisnadi@gmail.com>
2016-12-19 20:46:09 +0000

    input type added

--- greengraph_code/command.py
+++ greengraph_code/command.py
@@ -4,10 +4,10 @@ from greengraph_code.mkgraph import make_graph
 
 def load_image():
     parser = ArgumentParser(description = 'Visualize amount of green pixels between two locations')
-    parser.add_argument('--from', help='Start Location', dest='start')
-    parser.add_argument('--to', help='End Location', dest='end')
-    parser.add_argument('--steps', help='Steps between the two locations', dest='steps')
-    parser.add_argument('--out', help='Save as: (i.e. image.png)', dest='out')
+    parser.add_argument('--from', help='Start Location', dest='start', type=str)
+    parser.add_argument('--to', help='End Location', dest='end', type=str)
+    parser.add_argument('--steps', help='Steps between the two locations', dest='steps', type=int)
+    parser.add_argument('--out', help='Save as: (i.e. image.png)', dest='out', type=str)
     arguments = parser.parse_args()
 
     make_graph(arguments.start, arguments.end, arguments.steps, arguments.out)
