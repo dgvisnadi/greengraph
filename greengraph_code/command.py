6be1de7374b232df6d035b0f3f400bb3903711e3
Dimitri Visnadi <dgvisnadi@gmail.com>
2016-12-19 19:12:17 +0000

    Update command.py

--- greengraph_code/command.py
+++ greengraph_code/command.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from argparse import ArgumentParser
-from greengraph import make_graph
+from mkgraph import make_graph
 
 def load_image():
     parser = ArgumentParser(description = 'Visualize amount of green pixels between two locations')
