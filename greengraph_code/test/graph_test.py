6fe6c102838ff202928e29b8fc475cdb68a2bc54
Dimitri Visnadi <dgvisnadi@gmail.com>
2017-01-06 14:26:27 +0000

    test return message

--- greengraph_code/test/graph_test.py
+++ greengraph_code/test/graph_test.py
@@ -7,10 +7,11 @@ def Greengraph_test(start, end):
         greengraph_result = Greengraph(start, end)
         assert_equal(start, greengraph_result.start)
         assert_equal(end, greengraph_result.end)
-        return 'Test completed. All good.'
 
     # def geolocate_test():
 
     # def location_sequence_test():
 
     # def green_between_test():
+
+    return print('Test completed. All good.')
