6138bc9089cd8dc8526daa2afae0b90daa799f48
Dimitri Visnadi <dgvisnadi@gmail.com>
2017-01-06 13:09:50 +0000

    author and credit

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ setup(
     name = 'GreenGraph',
     version = '0.1.0',
     description= 'Plot showing green pixel distribution between two locations using Google Maps.',
+    author= 'Dimitri Visnadi'
+    credit= 'MPHYG001 - Univeristy College London'
     packages = find_packages(exclude=['*tests']),
     scripts = ['scripts/greengraph'],
-    install_requires = ['argparse', 'geopy', 'matplotlib', 'mock', 'numpy', 'requests']
+    install_requires = ['argparse', 'geopy', 'matplotlib', 'numpy', 'requests', 'seaborn']
 )
