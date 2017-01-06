8cb2262ccc66a78b7caef43881083f813e1bd00c
Dimitri Visnadi <dgvisnadi@gmail.com>
2017-01-06 13:00:34 +0000

    graph test file

---
+++ greengraph_code/test/graph_test.py
@@ -0,0 +1,15 @@
+from greengraph_code.graph import graph
+from nose.tools import assert_equal
+
+
+def Greengraph_test():
+    def Greengraph_init_test(start, end):
+    greengraph_result = Greengraph(start, end)
+    assert_equal(start, greengraph_result.start)
+    assert_equal(end, greengraph_result.end)
+
+    def geolocate_test():
+
+    def location_sequence_test():
+
+    def green_between_test():
